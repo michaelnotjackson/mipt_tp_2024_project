29e5e4459c0d1f7da21511b63b2ef6836782c949
makar.rasanov <rasanov.mv@phystech.edu>
2024-02-22 20:44:57 +0300

    Create basic room.h and equipment.h

--- include/equipment.h
+++ include/equipment.h
@@ -1,8 +1,90 @@
-//
-// Created by michael on 2/21/24.
-//
+#pragma once
 
-#ifndef TP_COURSE_SPRING24_PROJECT_EQUIPMENT_H
-#define TP_COURSE_SPRING24_PROJECT_EQUIPMENT_H
+#include <string>
+#include <utility>
 
-#endif //TP_COURSE_SPRING24_PROJECT_EQUIPMENT_H
+enum Properties {
+  FINESSE = 1 << 0,
+  HEAVY = 1 << 1,
+  LIGHT = 1 << 2,
+  LOADING = 1 << 3,
+  REACH = 1 << 4,
+  RANGE = 1 << 5,
+  SPECIAL = 1 << 6,
+  TWO_HANDED = 1 << 7,
+  VERSATILE = 1 << 8;
+};
+
+enum Equip {
+  BASEWEAPON,
+  RANGEWEAPON,
+  BASEARMOR,
+  POTION;
+};
+
+enum Damage {
+  SLASHING,
+  PIERCING,
+  BLUDGEONING,
+  POISON,
+  ACID,
+  FIRE,
+  COLD,
+  RADIANT,
+  NECROTIC,
+  LIGHTNING,
+  THUNDER,
+  FORCE,
+  PSYCHIC;
+};
+
+class BaseEquipment {
+ private:
+  std::string name;
+  Equip type;
+  int cost;
+  double weight;
+  int count;
+
+ public:
+  BaseEquipment();
+};
+
+class BaseWeapon: BaseEquipment {
+ private:
+  int damage;
+  Properties properties;
+  Damage type;
+
+ public:
+  BaseWeapon();
+};
+
+class RangedWeapon {
+ private:
+  int range;
+  std::string ammo_type;
+  //not string, but enum Ammo
+
+ public:
+  RangedWeapon();
+};
+
+class BaseArmor {
+ private:
+  int minimum_strength;
+  std::pair<CharacteristicType, int> modifier;
+  int armor_points;
+
+ public:
+  BaseArmor();
+};
+
+class Potion {
+ private:
+  int hp;
+  std::string true_name;
+
+ public:
+  Potion();
+};
