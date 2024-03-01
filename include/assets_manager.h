71c07d2319c4234aec614f7f77fa813967dbd775
Шейко Михаил <sheiko.ma@phystech.edu>
2024-03-01 19:27:38 +0300

    Add animations support for CAssetsManager

--- include/assets_manager.h
+++ include/assets_manager.h
@@ -1,5 +1,8 @@
+#pragma once
+
 #include <SDL.h>
 #include <SDL_image.h>
+#include <animation.h>
 
 #include <string>
 #include <unordered_map>
@@ -7,11 +10,20 @@
 class CAssetsManager {
  private:
   std::unordered_map<std::string, SDL_Texture*> textures;
+  std::unordered_map<std::string, CBaseAnimation*> animations;
 
  public:
   CAssetsManager();
 
-  void LoadTexture(const std::string& path, const std::string& name = "");
+  bool LoadTexture(const std::string& path, const std::string& name = "");
 
-  inline SDL_Texture* GetTexture(const std::string& name);
+  SDL_Texture* GetTexture(const std::string& name);
+
+  bool RegisterAnimation(CBaseAnimation* animation, const std::string& name);
+
+  CBaseAnimation* GetAnimation(const std::string& name);
+
+  ~CAssetsManager();
 };
+
+extern CAssetsManager assets_manager;
--- src/assets_manager.cpp
+++ src/assets_manager.cpp
@@ -1,22 +1,55 @@
 #include <SDL.h>
 #include <SDL_image.h>
+#include <animation.h>
 #include <assets_manager.h>
 #include <interface/CApp.h>
 
 CAssetsManager::CAssetsManager() : textures() {}
 
-void CAssetsManager::LoadTexture(const std::string& path,
+bool CAssetsManager::LoadTexture(const std::string& path,
                                  const std::string& name) {
   SDL_Texture* texture = IMG_LoadTexture(app.renderer, path.c_str());
 
+  if (texture == nullptr) {
+    SDL_Log("%s", SDL_GetError());
+    return false;
+  }
+
   if (name.empty()) {
     textures[path] = texture;
-    return;
+    return true;
   }
 
   textures[name] = texture;
+  return true;
 }
 
-inline SDL_Texture* CAssetsManager::GetTexture(const std::string& name) {
+SDL_Texture* CAssetsManager::GetTexture(const std::string& name) {
   return textures[name];
 }
+
+bool CAssetsManager::RegisterAnimation(CBaseAnimation* animation,
+                                       const std::string& name) {
+  if (animations.find(name) != animations.end()) {
+    SDL_LogError(1, "%s",
+                 "ERROR: Trying to register animation with name that was "
+                 "already taken");
+    return false;
+  }
+
+  animations[name] = animation;
+
+  return true;
+}
+
+CBaseAnimation* CAssetsManager::GetAnimation(const std::string& name) {
+  return animations[name];
+}
+
+CAssetsManager::~CAssetsManager() {
+  for (auto &[name, animation]: animations) {
+    delete animation;
+  }
+}
+
+CAssetsManager assets_manager;
