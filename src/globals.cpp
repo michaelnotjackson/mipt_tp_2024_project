15bac3821bd7ea385e715e2583952f3d0c33453a
Шейко Михаил <sheiko.ma@phystech.edu>
2024-03-11 21:05:29 +0300

    Adjusted movement speed

--- src/globals.cpp
+++ src/globals.cpp
@@ -10,6 +10,6 @@ ActionType g_current_action;
 int SCREEN_WIDTH = 640;
 int SCREEN_HEIGHT = 640;
 
-const int g_move_speed = 200;
+const int g_move_speed = 150;
 bool g_move_in_process = false;
 std::vector<PosType> g_current_path;
