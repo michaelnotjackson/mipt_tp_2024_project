40866b12e00207222cd1ed18ae7e6397d86bc032
kizotozxc <karvp1971@gmail.com>
2024-04-11 21:07:45 +0300

    reduce hp

--- src/engine/entity.cpp
+++ src/engine/entity.cpp
@@ -98,6 +98,8 @@ void AttackExecutor(PosType enemy_pos) {
             "animations/warriors/warrior_blue/attack2_right"));
     g_current_executor->PlayAnimation(assets_manager.GetAnimation(
         "animations/warriors/warrior_blue/attack2_right"));
+    std::get<int>(g_current_room.field[enemy_pos.y][enemy_pos.x]
+                      ->entity_on->props["i_health"]) -= 5;
   }
   g_current_action = ActionType::FREE;
 }
